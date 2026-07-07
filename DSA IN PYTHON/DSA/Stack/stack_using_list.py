e1262c8fe246a24f7b28ad0aa732d52d72b3f766
Ritesh <riteshrawat1r@gmail.com>
2026-07-07 11:28:26 +0530

    ADDED STACK DATA SCTRUCTURE

---
+++ DSA IN PYTHON/DSA/Stack/stack_using_list.py
@@ -0,0 +1,60 @@
+'''class stack:
+    def __init__(self):
+        self.item = []      
+    def is_empty(self):
+        return len(self.item) == 0   
+    def push(self, data):
+        self.item.append(data)
+    def pop(self):
+        if not self.is_empty():
+            return self.item.pop()
+        else:
+            raise IndexError("Stack is empty")
+    def peek(self):
+        if not self.is_empty():
+            return self.item[-1]
+        else:
+            raise IndexError("stack is empty")
+    def size(self):
+        return len(self.item)
+    
+s1=stack()
+s1.push(10)
+s1.push(20)
+s1.push(30)
+print("Top element is:",s1.peek())
+print("Stack size is:",s1.size())
+print("pop element is:", s1.pop())
+print("Stack size is:",s1.size())'''
+
+
+class stack:
+    def __init__(self):
+        self.item = []
+        
+    def is_empty(self):
+        return len(self.item) == 0
+    def push(self, data):
+        self.item.append(data)
+    def pop(self):
+        if not self.is_empty():
+            return self.item.pop()
+        else:
+            raise IndexError("stack is empty")
+    def peek(self):
+        if not self.is_empty():
+            return self.item[-1]
+        else:
+            raise IndexError("Stack is empty")
+    def size(self):
+        return len(self.item)
+
+s1=stack()
+s1.push(10)
+s1.push(20)
+s1.push(30)
+s1.push(40)
+s1.pop()
+print("Top element is:",s1.peek())
+print("Stack size is:",s1.size())
+print("pop element is:", s1.pop())
